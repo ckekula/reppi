a269176bb49ce61cf8b7bd6688bda5b91d370a34
Chamuditha Kekulawala <chamukeku@gmail.com>
2026-07-09 21:18:52 +0530

    bugfix

--- src/reppi/dictionary/ksvd/ksvd.py
+++ src/reppi/dictionary/ksvd/ksvd.py
@@ -29,7 +29,7 @@ import numpy as np
 from reppi.base import BaseDictionaryLearner
 from reppi.exceptions import DictionaryLearningError
 from reppi.sparse.omp.omp import OMP, batch_omp
-from reppi.sparse.omp.utils import _check_dict_normalized
+from reppi.sparse.utils import _check_dict_normalized
 from reppi.sparse.utils import col_norms_squared, normalize_columns, rep_error_squared
 
 from reppi.dictionary.ksvd.utils import _optimize_atom, _clear_dict
